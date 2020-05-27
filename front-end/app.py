a9472d2efdf9a7d33480bf3c951f067bc403990c
Dorta1 <59895896+Dorta1@users.noreply.github.com>
2020-05-26 19:45:40 -0500

    Dummy graph

--- front-end/app.py
+++ front-end/app.py
@@ -1,14 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, Markup, render_template
 
 app = Flask(__name__)
 
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    bar_labels=labels
+    bar_values=values
+    return render_template('index.html', chart_title='Bitcoin Monthly Price in USD', max=17000, labels=bar_labels, values=bar_values)
 
 @app.route('/demographics')
 def demographics():
     return render_template('demographics.html')
+
+def line():
+    line_labels=labels
+    line_values=values
+    return render_template('demographics.html', title='Bitcoin Monthly Price in USD', max=17000, labels=line_labels, values=line_values)
 
 @app.route('/offense_category')
 def offense_category():
