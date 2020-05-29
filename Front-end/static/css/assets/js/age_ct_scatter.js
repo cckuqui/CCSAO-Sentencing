67526093de2b066be69476da8bf80555303efdf0
Dorta1 <59895896+Dorta1@users.noreply.github.com>
2020-05-29 00:48:50 -0500

    tried to load json to index.html

---
+++ Front-end/static/css/assets/js/age_ct_scatter.js
@@ -0,0 +1,28 @@
+var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: 'markers',
+    type: 'scatter'
+  };
+  
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: 'markers',
+    type: 'scatter'
+  };
+  
+  var layout = {
+    title: "Basic Scatter Plot",
+    width: 950,
+    height: 350,
+    paper_bgcolor: 'rgba(0,0,0,0)',
+    plot_bgcolor: 'rgba(0,0,0,0)', 
+    font: {
+      color: 'white'
+  }
+  };
+  
+  var data = [trace1, trace2];
+  
+  Plotly.newPlot("age_ct_scatter", data, layout);
