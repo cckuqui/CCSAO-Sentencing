7d73aebb7fa420a7c5bda895469f5238bcf32ce7
patelpurvip <patel.purvi.p@gmail.com>
2020-05-30 01:01:18 -0500

    scatterplot progress

--- Front-end/static/css/assets/js/loc_ct_scatter.js
+++ Front-end/static/css/assets/js/loc_ct_scatter.js
@@ -1,16 +1,26 @@
+d3.json("./static/css/assets/js/scatter_length.json").then((importedData) => {
+  console.log(importedData);
+  var length_days = []
+  var years = []
+  length_days = Object.values(importedData.length_of_case_in_days);
+  years = Object.values(importedData.year);
+  console.log(length_days);
+  console.log(years);
+
+
 var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
+    x: length_days,
+    y: years,
     mode: 'markers',
     type: 'scatter'
   };
   
-  var trace2 = {
-    x: [2, 3, 4, 5],
-    y: [16, 5, 11, 9],
-    mode: 'markers',
-    type: 'scatter'
-  };
+  // var trace2 = {
+  //   x: [2, 3, 4, 5],
+  //   y: [16, 5, 11, 9],
+  //   mode: 'markers',
+  //   type: 'scatter'
+  // };
   
   var layout = {
     title: "Basic Scatter Plot",
@@ -23,6 +33,8 @@ var trace1 = {
   }
   };
   
-  var data = [trace1, trace2];
+  var data = [trace1];
   
   Plotly.newPlot("loc_ct_scatter", data, layout);
+
+});
