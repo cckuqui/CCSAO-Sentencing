f2737e84395914f3f5dccd4664fb46a426b0a860
Cris B <cristina.bardan@gmail.com>
2020-05-29 20:05:38 -0500

    query and json for boxplot in offense

--- Back-end/new_queries.sql
+++ Back-end/new_queries.sql
@@ -18,7 +18,7 @@ from
 	left join sentences s
 		on r.sentence_id = s.sentence_id
 	group by (
-		o.offense_category, 
+		o.offense_category,
 		s.sentence_type, 
 		r.case_participant_id
 	)) fr
@@ -26,3 +26,27 @@ left join participants pa
 	on fr.case_participant_id = pa.case_participant_id;
 
 -- Second query for the offense boxplot
+select
+	s.year,
+	s.month,
+	fr.offense_category,
+	s.sentence_type,
+	fr.court_name
+from
+	(select
+	 	r.sentence_id,
+	 	o.offense_category,
+	 	co.court_name
+	 from results r
+	 left join courts co
+	 	on r.court_id = co.court_id
+	 left join offenses o
+	 	on r.offense_id = o.offense_id
+	 group by (
+	 	r.sentence_id,
+	 	o.offense_category,
+	 	co.court_name
+	)) fr
+left join sentences s
+	on s.sentence_id = fr.sentence_id
+where s.month !=0;
